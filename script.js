b7888f4517ca1feb529c0007783c0039eb7d254c
sheikhmhummadhasan <hasan2001onlin@gmail.com>
2026-03-04 17:39:40 +0600

    add some toggel style

--- script.js
+++ script.js
@@ -18,13 +18,18 @@ let lessonfunction = () => {
         let getIdfromLi = document.querySelectorAll(".event")
 
         getIdfromLi.forEach((f) => {
-            console.log(f)
             f.addEventListener("click", () => {
+                document.querySelectorAll(".event").forEach((elem) =>{
+                    elem.classList.remove("action")
+                })
+
+                f.classList.add("action")
+                
                 if (f.innerText.trim() == "7.Mastering Vocabulary") {
                     word_continer.classList.add("bg-gray-200")
                     word_contibner_item.classList.add("hidden")
                     word_contibner_item2.classList.remove("hidden")
-                    
+
                 }
 
                 if (f.innerText.trim() == "6.Mastering Vocabulary") {
@@ -38,9 +43,9 @@ let lessonfunction = () => {
                     word_contibner_item.classList.add("hidden")
                     word_contibner_item2.classList.remove("hidden")
                 }
-                
+
                 if (f.innerText == "1.Basic Vocabulary") {
-                    if(word_continer.innerHTML != ""){
+                    if (word_continer.innerHTML != "") {
                         word_continer.innerHTML = ""
                     }
                     word_continer.classList.remove("bg-gray-200")
@@ -50,7 +55,7 @@ let lessonfunction = () => {
                 }
 
                 if (f.innerText == "2.Everyday Words") {
-                    if(word_continer.innerHTML != ""){
+                    if (word_continer.innerHTML != "") {
                         word_continer.innerHTML = ""
                     }
                     word_continer.classList.remove("bg-gray-200")
@@ -60,7 +65,7 @@ let lessonfunction = () => {
                 }
 
                 if (f.innerText == "3.Intermediate Vocabulary") {
-                    if(word_continer.innerHTML != ""){
+                    if (word_continer.innerHTML != "") {
                         word_continer.innerHTML = ""
                     }
                     word_continer.classList.remove("bg-gray-200")
@@ -70,7 +75,7 @@ let lessonfunction = () => {
                 }
 
                 if (f.innerText == "4.Advanced Vocabulary") {
-                    if(word_continer.innerHTML != ""){
+                    if (word_continer.innerHTML != "") {
                         word_continer.innerHTML = ""
                     }
                     word_continer.classList.remove("bg-gray-200")
@@ -78,6 +83,7 @@ let lessonfunction = () => {
                     word_contibner_item.classList.add("hidden")
                     cardFunction4()
                 }
+                
                 console.log(f)
             })
         })
@@ -113,7 +119,7 @@ function cardFunction() {
             </div>
         </div>
             `
-        // console.log(e)
+            // console.log(e)
         })
     };
     fetchi()
@@ -144,7 +150,7 @@ function cardFunction2() {
                 <button class="bg-[#1A91FF30] p-3 rounded-full hover:bg-[#1A91FF] spker-btn"><i class="fa-solid fa-volume-high "></i></button>
             </div>
         </div>`
-        // console.log(e)
+            // console.log(e)
         })
     };
     fetchi2()
@@ -175,7 +181,7 @@ function cardFunction3() {
                 <button class="bg-[#1A91FF30] p-3 rounded-full hover:bg-[#1A91FF] spker-btn"><i class="fa-solid fa-volume-high "></i></button>
             </div>
         </div>`
-        // console.log(e)
+            // console.log(e)
         })
     };
     fetchi3()
@@ -206,18 +212,37 @@ function cardFunction4() {
                 <button class="bg-[#1A91FF30] p-3 rounded-full hover:bg-[#1A91FF] spker-btn"><i class="fa-solid fa-volume-high "></i></button>
             </div>
         </div>`
-        // console.log(e)
+            // console.log(e)
         })
     };
     fetchi3()
 }
 
-function infoOfword(){
-    let url = "https://openapi.programming-hero.com/api/word/5"
-    let fetching =async () =>{
+
+function infoOfword() {
+    let info = document.querySelector(".info")
+    let url = "https://openapi.programming-hero.com/api/words/all"
+    let fetching = async () => {
         let res = await fetch(url);
         let resp = await res.json();
-        console.log(resp.data)
+        // console.log(resp.data)
+        resp.data.forEach((e) => {
+            info.innerHTML = `<div class="">
+            <h2 class="text-2xl font-semibold">${e.word} (<i class="fa-solid fa-microphone-lines"></i>:${e.pronunciation})</h2>
+            <h3 class="font-semibold mt-2 mb-2">Meaning</h3>
+            <p class="font-semibold mb-3">${e.meaning}</p>
+            <h2 class="font-semibold mb-2">Example</h2>
+            <p class="mb-3">The kids were eager to open their gifts.</p>
+            <p class="mb-2 font-semibold">সমার্থক শব্দ গুলো</p>
+            <div class="btns">
+                <button class="btn-1 bg-sky-200 py-2 px-3 rounded-sm">Enthusiastic</button>
+                <button class="btn-1 bg-sky-200 py-2 px-3 rounded-sm">excited</button>
+                <button class="btn-1 bg-sky-200 py-2 px-3 rounded-sm">keen</button>
+            </div>
+            <button class="btn bg-[#422AD5] text-[#E0E7FF] mt-4 border-none">Complete Learning</button>
+        </div>`
+        // console.log(e)
+        })
     };
     fetching()
 
@@ -226,17 +251,20 @@ infoOfword()
 
 let info = document.querySelector(".info");
 document.querySelector("#word-container")
-.addEventListener("click", (e) => {
-    if (e.target.closest(".info-btn")) {
-        info.classList.remove("hidden")
-        info.classList.add("opacity-100")
-    }
-    
-});
-let body = document.querySelector("body");
+    .addEventListener("click", (e) => {
+        if (e.target.closest(".info-btn")) {
+            info.classList.remove("hidden");
+            info.classList.add("opacity-100");
+        }
+        // console.log(e)
+        infoOfword()
+    });
 
-body.addEventListener("click",(e)=>{
-    if(!e.target.closest(".info-btn") && !e.target.closest(".popup-card")){
+let body = document.querySelector("body");
+body.addEventListener("click", (e) => {
+    if (!e.target.closest(".info-btn") && !e.target.closest(".popup-card")) {
         info.classList.add("hidden")
     }
 })
+
+
